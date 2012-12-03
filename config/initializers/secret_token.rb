9d9e6fd1b5a369ebc50a625c1a04659c2a1694aa
Walter Miller <walter@waltermiller.ca>
2012-12-03 10:59:15 -0500

    Third attempt at backend, I have a good feeling about this one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobitrack3::Application.config.secret_token = 'da059a6b41af2f5df68c9d37a421bdfbfa0806fba0728b43fdd26fe4976a7ac5f07b666047beebdaa58cdd2de8b8a130cf57967ce038c7e085743ee0802f728c'
