c17bc4ddda069217f366d68837d6b553af362032
Walter Miller <walter@waltermiller.ca>
2012-12-04 16:13:28 -0500

    Added 50 person limit so it's more responsive

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,7 @@ class PeopleController < ApplicationController
   # GET /people
   # GET /people.json
   def index
-    @people = Person.all
+    @people = Person.all :limit => 50
     render json: @people
   end
 
