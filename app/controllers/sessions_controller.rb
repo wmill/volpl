c9802a8907b8f678900506516195cbc7b8f1f481
Walter Miller <walter@waltermiller.ca>
2012-12-11 11:13:20 -0500

    Made usernames case and whitespace insensitive.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_user_name(params[:user_name])
+    user_name = params[:user_name].downcase.strip
+    user = User.find_by_user_name(user_name)
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to "/"
