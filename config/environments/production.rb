ab2f5e5ab0721221b62f9b377fab898ea2c2c765
Walter Miller <walter@waltermiller.ca>
2013-01-11 02:53:59 -0500

    Tweaks to fix assets for 3.1.10 production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Mobitrack3::Application.configure do
   config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = false
@@ -43,7 +43,7 @@ Mobitrack3::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jquery.mobile-1.2.0.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
