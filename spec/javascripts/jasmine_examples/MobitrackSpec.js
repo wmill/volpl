0ae1a441be20284cacaf11429040f7a0b3ba97d7
Walter Miller <walter@waltermiller.ca>
2013-04-08 11:15:44 -0400

    Adding in template changes

---
+++ spec/javascripts/jasmine_examples/MobitrackSpec.js
@@ -0,0 +1,8 @@
+describe('mobitrack', function(){
+    it("should have templates", function(){
+        HandlebarsTemplates
+    });
+    
+    var person = {"created_at":"2012-12-08T04:30:08Z","first_name":"Tiffany","id":204,"last_name":"Hanson","riding":"Ajax--Pickering","updated_at":"2012-12-08T04:30:08Z"};
+    
+});
